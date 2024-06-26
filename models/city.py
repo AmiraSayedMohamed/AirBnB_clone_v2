f1fef796d6b6bb02ca1b0cf3f9baad2213fad7e7
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-06-26 08:49:48 -0700

    Add

--- models/city.py
+++ models/city.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-import os
+"""
+City module for the models package
+"""
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-
 from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship('Place', cascade='all, delete, delete-orphan', backref='cities')
-    else:
-        name = ''
-        state_id = ''
+    """
+    City class inherits from BaseModel and Base
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    state = relationship("State", back_populates="cities")
+
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize City instance
+        """
+        super().__init__(*args, **kwargs)
+
+
+Base.metadata.create_all(engine)
 
--- models/state.py
+++ models/state.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-import os
+"""
+State module for the models package
+"""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-
 from models.base_model import BaseModel, Base
-from models.city import City
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = 'states'
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all, delete, delete-orphan', backref='state')
-    else:
-        name = ''
+    """
+    State class inherits from BaseModel and Base
+    """
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade="all, delete", back_populates="state")
 
-        @property
-        def cities(self):
-            """Returns the list of City instances with state_id equals to the current State.id"""
-            from models import storage
-            return [city for city in storage.all(City).values() if city.state_id == self.id]
+    def __init__(self, *args, **kwargs):
+        """
+        Initialize State instance
+        """
+        super().__init__(*args, **kwargs)
+
+
+Base.metadata.create_all(engine)
 
