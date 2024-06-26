777771cf3249da4d8e1ea77c4c71a8b4538b8ecd
AmiraSayedMohamed <amira.sayedza@gmail.com>
2024-06-26 04:07:00 -0700

    add

--- models/amenity.py
+++ models/amenity.py
@@ -1,24 +1,8 @@
-#!/usr/bin/python3
-""" Amenity Module for HBNB project """
-import os
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
-from models.base_model import BaseModel, Base
-from models.place import place_amenity
+class Amenity(BaseModel):
+    name = str
 
-
-class Amenity(BaseModel, Base):
-    """Represents an amenity data set."""
-    __tablename__ = 'amenities'
-    name = Column(
-        String(128), nullable=False
-    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
-
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        place_amenities = relationship(
-            'Place',
-            secondary=place_amenity,
-            back_populates='amenities'
-        )
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
--- models/base_model.py
+++ models/base_model.py
@@ -1,65 +1,24 @@
-#!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
-import os
-import uuid
-from datetime import datetime
-from sqlalchemy import Column, String, DATETIME
-from sqlalchemy.ext.declarative import declarative_base
+import json
 from models import storage
 
-Base = declarative_base()
+class FileStorage:
+    """A file-based storage engine for AirBnB clone data."""
 
+    def __init__(self, file_path="file.json"):
+        self.file_path = file_path
 
-class BaseModel:
-    """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True, unique=True)
-    created_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
-    updated_at = Column(DATETIME, nullable=False, default=datetime.utcnow())
+    def all(self, cls=None):
+        """Retrieves all objects from storage."""
+        objects = []
+        try:
+            with open(self.file_path, 'r') as f:
+                data = json.load(f)
+                for obj_val in data.values():
+                    obj = cls(**obj_val) if cls else BaseModel(**obj_val)
+                    objects.append(obj)
+        except FileNotFoundError:
+            pass
+        return objects
 
-    def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-        else:
-            for key, value in kwargs.items():
-                if key != '__class__':
-                    if key in ('created_at', 'updated_at'):
-                        setattr(self, key, datetime.fromisoformat(value))
-                    else:
-                        setattr(self, key, value)
-            if 'id' not in kwargs:
-                setattr(self, 'id', str(uuid.uuid4()))
-            if 'created_at' not in kwargs:
-                setattr(self, 'created_at', datetime.now())
-            if 'updated_at' not in kwargs:
-                setattr(self, 'updated_at', datetime.now())
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls_name = self.__class__.__name__
-        return '[{}] ({}) {}'.format(cls_name, self.id, self.__dict__)
-
-    def delete(self):
-        """Deletes this BaseModel instance from the storage"""
-        storage.delete(self)
-
-    def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        self.updated_at = datetime.now()
-        storage.new(self)
-        storage.save()
-
-    def to_dict(self):
-        """Convert instance into dict format"""
-        res = {}
-        for key, value in self.__dict__.items():
-            if key != '_sa_instance_state':
-                if isinstance(value, datetime):
-                    res[key] = value.isoformat()
-                else:
-                    res[key] = value
-        res['__class__'] = self.__class__.__name__
-        return res
+    # ... other storage methods (save, new, delete, etc.)
 
